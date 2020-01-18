34d6785ea98621f4cadeb7db9f1480f2f83e6492
iivansmile <Ivan_turla@yahoo.com>
2020-01-18 17:35:28 +0800

    SevenSegment

---
+++ SevenSegmentTurla/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(68, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(31, 102)
+        Me.Button2.TabIndex = 15
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(105, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(123, 34)
+        Me.Button1.TabIndex = 14
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Button7
+        '
+        Me.Button7.Enabled = False
+        Me.Button7.Location = New System.Drawing.Point(105, 117)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(123, 35)
+        Me.Button7.TabIndex = 20
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Enabled = False
+        Me.Button6.Location = New System.Drawing.Point(105, 209)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(123, 34)
+        Me.Button6.TabIndex = 19
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Enabled = False
+        Me.Button5.Location = New System.Drawing.Point(68, 141)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(31, 102)
+        Me.Button5.TabIndex = 18
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(234, 141)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(31, 102)
+        Me.Button4.TabIndex = 17
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(234, 23)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(31, 102)
+        Me.Button3.TabIndex = 16
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 268)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ SevenSegmentTurla/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+    Dim Second As Int32 = -1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Second = Second + 1
+        If (Second = 1) Then
+            Button1.BackColor = Color.White
+            Button2.BackColor = Color.White
+            Button3.BackColor = Color.Blue
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.White
+            Button6.BackColor = Color.White
+            Button7.BackColor = Color.White
+        ElseIf (Second = 2) Then
+            Button1.BackColor = Color.Blue
+            Button2.BackColor = Color.White
+            Button3.BackColor = Color.Blue
+            Button4.BackColor = Color.White
+            Button5.BackColor = Color.Blue
+            Button6.BackColor = Color.Blue
+            Button7.BackColor = Color.Blue
+
+        ElseIf (Second = 3) Then
+            Button1.BackColor = Color.Blue
+            Button2.BackColor = Color.White
+            Button3.BackColor = Color.Blue
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.White
+            Button6.BackColor = Color.Blue
+            Button7.BackColor = Color.Blue
+
+        ElseIf (Second = 4) Then
+            Button1.BackColor = Color.White
+            Button2.BackColor = Color.Blue
+            Button3.BackColor = Color.Blue
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.White
+            Button6.BackColor = Color.White
+            Button7.BackColor = Color.Blue
+
+        ElseIf (Second = 5) Then
+            Button1.BackColor = Color.Blue
+            Button2.BackColor = Color.Blue
+            Button3.BackColor = Color.White
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.White
+            Button6.BackColor = Color.Blue
+            Button7.BackColor = Color.Blue
+
+        ElseIf (Second = 6) Then
+            Button1.BackColor = Color.Blue
+            Button2.BackColor = Color.Blue
+            Button3.BackColor = Color.White
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.Blue
+            Button6.BackColor = Color.Blue
+            Button7.BackColor = Color.Blue
+
+        ElseIf (Second = 7) Then
+            Button1.BackColor = Color.Blue
+            Button2.BackColor = Color.Blue
+            Button3.BackColor = Color.Blue
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.White
+            Button6.BackColor = Color.White
+            Button7.BackColor = Color.White
+
+        ElseIf (Second = 8) Then
+            Button1.BackColor = Color.Blue
+            Button2.BackColor = Color.Blue
+            Button3.BackColor = Color.Blue
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.Blue
+            Button6.BackColor = Color.Blue
+            Button7.BackColor = Color.Blue
+
+        ElseIf (Second = 9) Then
+            Button1.BackColor = Color.Blue
+            Button2.BackColor = Color.Blue
+            Button3.BackColor = Color.Blue
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.White
+            Button6.BackColor = Color.Blue
+            Button7.BackColor = Color.Blue
+
+        Else
+            Button1.BackColor = Color.Blue
+            Button2.BackColor = Color.Blue
+            Button3.BackColor = Color.Blue
+            Button4.BackColor = Color.Blue
+            Button5.BackColor = Color.Blue
+            Button6.BackColor = Color.Blue
+            Button7.BackColor = Color.White
+            Second = 0
+        End If
+    End Sub
+End Class
